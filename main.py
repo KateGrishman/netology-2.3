13f5dce9ca58a01b51501647c18f0db8c08f7d3b
Kate Grishman <47010624+KateGrishman@users.noreply.github.com>
2019-02-05 21:32:43 +0300

    Added functions

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import sys
 
-a = input('Введите выражение: ')
-expression = a.split()
-assert len(expression) == 3, 'Неверное количество аргументов!'
-
 
 def read_number(expr):
     try:
@@ -15,28 +11,38 @@ def read_number(expr):
     return num
 
 
-num1 = read_number(expression[1])
-num2 = read_number(expression[2])
+def read_expression_str():
+    a = input('Введите выражение: ')
+    expression = a.split()
+    assert len(expression) == 3, 'Неверное количество аргументов!'
+    return expression
 
-oper = expression[0]
 
-operation = ['+', '-', '*', '/']
-assert oper in operation, 'Недопустимая операция'
+def parse_arguments(expression):
+    oper = expression[0]
+    num1 = read_number(expression[1])
+    num2 = read_number(expression[2])
+    return oper, num1, num2
 
-if oper == '+':
-    result = num1 + num2
-elif oper == '-':
-    result = num1 - num2
-elif oper == '*':
-    result = num1 * num2
-elif oper == '/':
-    try:
-        result = num1 / num2
-    except ZeroDivisionError:
-        print('Нельзя делить на ноль!')
-        sys.exit(1)
 
-print(result)
+def evaluate_expression(oper, num1, num2):
+    operation = ['+', '-', '*', '/']
+    assert oper in operation, 'Недопустимая операция'
+    if oper == '+':
+        result = num1 + num2
+    elif oper == '-':
+        result = num1 - num2
+    elif oper == '*':
+        result = num1 * num2
+    elif oper == '/':
+        try:
+            result = num1 / num2
+        except ZeroDivisionError:
+            print('Нельзя делить на ноль!')
+            sys.exit(1)
+    print(result)
+
+evaluate_expression(*parse_arguments(read_expression_str()))
 
 
 
